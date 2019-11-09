6f864da70f0ea85d2bab14718c6770180ee77351
Jen <jmbabaran@gmail.com>
2019-11-09 12:48:20 +1100

    Online quiz system backend upload

---
+++ backend/Controllers/QuestionsController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    //[ApiController]
+    public class QuestionsController : ControllerBase
+    {
+
+        readonly QuizContext context;
+
+        public QuestionsController(QuizContext context)
+        {
+            this.context = context;
+        }
+
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Models.Question> Get()
+        {
+            return context.Questions;
+        }
+
+        [HttpGet("{quizId}")]
+        public IEnumerable<Models.Question> Get([FromRoute] int quizId)
+        {
+            return context.Questions.Where(q => q.QuizId == quizId);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Models.Question question)
+        {
+            var quiz = context.Quiz.SingleOrDefault(q => q.ID == question.QuizId);
+            if (quiz == null)
+            {
+                return NotFound();
+            }
+            context.Add(question);
+            await context.SaveChangesAsync();
+            return Ok(question);
+        }
+
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Models.Question question)
+        {
+            if (id != question.ID)
+            {
+                return BadRequest();
+            }
+            context.Entry(question).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            
+
+            return Ok(question);
+        }
+
+
+    }
+}
